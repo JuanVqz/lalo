c6a3c3e51a366630d79c4c1b54c2cc54c6bf8cd9
Juan Vasquez <juan@ombulabs.com>
2023-01-15 11:06:13 -0600

    chore(style): add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "config/routes.rb",
+    "app/views/**/*",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
